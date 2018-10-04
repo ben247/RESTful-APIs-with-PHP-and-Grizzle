a3490af78f67de828addefafe1eec65597c67a51
ben whalley <ben247@gmail.com>
2018-10-04 20:15:42 +0100

    Query strings to build request URI

---
+++ query.php
@@ -0,0 +1,20 @@
+<?php
+//Guzzle RESTful API request
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+$client = new Client();
+
+$response = $client->request(
+    'GET',
+    'http://jsonplaceholder.typicode.com/posts',
+    [
+        'query' => [
+            'userId' => 1
+        ]
+    ]
+);
+
+var_dump($response);
+echo $response->getBody();
+
+?>
