fcc8ecc922d91c86d6a6f39874872c1c4a0c7664
ben whalley <ben247@gmail.com>
2018-10-04 19:54:56 +0100

    First Guzzle async/promise

---
+++ async.php
@@ -0,0 +1,25 @@
+<?php
+//Async request - returns promise
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+use GuzzleHttp\Prs7\Response;
+use GuzzleHttp\Exception\RequestException;
+$client = new Client();
+
+$promise = $client->requestAsync(
+    'GET',
+    'http://jsonplaceholder.typicode.com/posts/1'
+);
+
+$promise->then(
+    function (Response $resp) {
+        echo $resp->getBody();
+    },
+    function (RequestException $e) {
+        echo $e->getMessage();
+    }
+);
+
+$promise->wait();
+
+?>
--- request.php
+++ request.php
@@ -4,6 +4,15 @@ require 'vendor/autoload.php';
 use GuzzleHttp\Client;
 $client = new Client(['base_uri' => 'http://jsonplaceholder.typicode.com/']);
 
+//Request 1
+$response = $client->get(
+    'posts/1'
+);
+
+var_dump($response);
+echo $response->getBody();
+
+//Request 2
 $response = $client->get(
     'https://httpbin.org/ip'
 );
