1a7be0740e65d311e40b307956d46eb321151446
ben whalley <ben247@gmail.com>
2018-10-05 16:47:38 +0100

    Basic Guzzle response

---
+++ response.php
@@ -0,0 +1,15 @@
+<?php
+//Guzzle RESTful API request
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+$client = new Client();
+
+$response = $client->request(
+    'GET',
+    'http://jsonplaceholder.typicode.com/posts/1'
+);
+
+var_dump($response);
+$body
+
+?>
