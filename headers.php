5a250bf96618237aa61d9f3cbe8f0edd611ac02b
ben whalley <ben247@gmail.com>
2018-10-07 12:40:54 +0100

    formating api repsonse headers

--- headers.php
+++ headers.php
@@ -17,4 +17,11 @@ foreach ($headers as $name => $value) {
     echo "{$name}: {$value}\r\n";
 }
 
+$type = $response->getHeader('Content-Type');
+if (in_array('application/json; charset-utf-8', $type)) {
+    $body = json_decode($response->getBody());
+} else {
+    $body = $response->getBody();
+}
+var_dump($body);
 ?>
